fd8d58dcb7a43ac778c5799357ad78affbd3c974
rizqyhbb <rizqyhbb@gmail.com>
2023-07-19 10:33:43 +0700

    change withAuth conditional checking

--- src/helper/withAuth.ts
+++ src/helper/withAuth.ts
@@ -5,27 +5,24 @@ const withAuth = (getServerSidePropsFunction: GetServerSideProps) => {
   return async (ctx: GetServerSidePropsContext) => {
     const { req } = ctx;
 
-    if (req.headers.cookie) {
-      const cookies = cookie.parse(req.headers.cookie);
-      const currentAuth = cookies.auth;
+    const isAuthenticated = Boolean(req.cookies.auth);
 
-      if (currentAuth && req.url?.includes("/signin")) {
-        return {
-          redirect: {
-            permanent: false,
-            destination: "/dashboard",
-          },
-        };
-      }
+    if (isAuthenticated && req.url?.includes("/signin")) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: "/dashboard",
+        },
+      };
+    }
 
-      if (!currentAuth && !req.url?.includes("/signin")) {
-        return {
-          redirect: {
-            permanent: false,
-            destination: "/signin",
-          },
-        };
-      }
+    if (!isAuthenticated && !req.url?.includes("/signin")) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: "/signin",
+        },
+      };
     }
     return await getServerSidePropsFunction(ctx);
   };
